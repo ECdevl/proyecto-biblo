c6c57984a355461ff84c3203509c52b14ad298e8
TamaraZalavsky82 <tamaraanabellazaslavsky@gmail.com>
2025-04-21 11:21:10 -0300

    algunas cositas

---
+++ src/pages/dashboardUser.jsx
@@ -0,0 +1,11 @@
+//crear la UI  de como seria el dasboard del Administrador
+
+import React from 'react'
+
+function dashboardUser() {
+  return (
+    <div>dashboardUser</div>
+  )
+}
+
+export default dashboardUser
---
+++ src/pages/login.jsx
@@ -0,0 +1,11 @@
+//Crear un inicio de sesion en el cual tenga una imagen del logo de la bibloteca del lado izquierdo de la pantalla y del lado derecho de la pantalla los input
+
+import React from 'react'
+
+function login() {
+  return (
+    <div>login</div>
+  )
+}
+
+export default login
